aed1adfbd149b70ea31caa1cab95cac52f801b75
Italo61-dev <italosousa42541@gmail.com>
2023-08-08 18:58:05 -0300

    Create funcoes-arraw.js

---
+++ desenvolvimento-em-javascript/aula004/funcoes-arraw.js
@@ -0,0 +1,16 @@
+//functions
+function soma (a, b){
+    let operação = a + b;
+    return operação
+}
+
+//chamaada da função
+let resultado = soma(2, 2);
+console.log(resultado);
+///////////////////
+//Arraw function
+const soma2 = (a, b) => a + b;
+
+console.log(soma2(2,5))
+
+
