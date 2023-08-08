0aa950da21278217f44c1749d6092dde424a7aa9
Italo61-dev <italosousa42541@gmail.com>
2023-08-08 15:17:41 -0300

    Create while.js

---
+++ desenvolvimento-em-javascript/aula003/while.js
@@ -0,0 +1,39 @@
+//ESTRUTURA DE REPETIÇÃO WHILE "ENQUANTO"
+// declarar variável de entrada
+ 
+// while(condição de parada){
+//     comando 1
+//     comando 2
+//     ...
+//     Comando n
+ 
+//     incrementar variável de entrada
+// }
+
+// i = 0                // declaração do valor de início
+ 
+// while(let = i < 5){        // condição de parada
+//     console.log("while:", i);  // imprime os dados na tela
+//     i++;             // incrementação: +1
+// }
+
+// for(i = 0; i < 5; i++){
+//     console.log("for:", i)
+// }
+
+//ESTRUTURA DE REPETIÇÃO DO...WHILE "fazer equanto"
+
+i = 10;
+
+do{
+    console.log("do:", i);
+    i++
+}while(i < 5);
+
+i = 10;
+
+while(i < 5){
+    console.log("while", i)
+    i++
+}
+
