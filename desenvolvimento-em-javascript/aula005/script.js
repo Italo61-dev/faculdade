e76483423f484cad6bf9b7f262d5fd43c592fd25
Italo61-dev <italosousa42541@gmail.com>
2023-08-09 18:45:26 -0300

    Update script.js

--- desenvolvimento-em-javascript/aula005/script.js
+++ desenvolvimento-em-javascript/aula005/script.js
@@ -1,28 +1,35 @@
 // getElementById
-    var titulo = document.getElementById("logo");
+    //var titulo = document.getElementById("logo");
 
     //titulo.innerHTML = "<b><i>AAAAAAAAAA" // -> muda o id selecionado diretamente no html
     //titulo.innerText = "<b><i>AAAAAAAAAA" -> muda o texto do id
     //console.log(titulo.innerHTML);
 
 // getElementsByTagName
-    var paragrafos = document.getElementsByTagName("p");
+    // var paragrafos = document.getElementsByTagName("p");
 
-    console.log(paragrafos[0].innerText)
+    // console.log(paragrafos[0].innerText)
 
     //mudando a cor do header
     window.onload = ()=>{
-    
+        
         var selecionado = document.getElementsByTagName('header');
         selecionado = selecionado[0];
-
+        var texto_selecionado=selecionado.querySelectorAll("#logo")[0];// querySelectorAll
+        
+        
         selecionado.style.backgroundColor = 'purple';
         selecionado.style.fontSize = '3em';
-        selecionado.style.color = 'black';
+        texto_selecionado.style.color = 'black';
         selecionado.style.width = '100vw';
     }
-
     
-// getElementsByClassName
+    
+    // getElementsByClassName
+    var texto = document.getElementsByClassName("texto")[1]
+    texto.style.color = "red";
 
-// querySelectorAll
+    // querySelectorAll
+    var topo = document.querySelectorAll('header')[0];
+
+    topo.style.backgroundColor = "black";
