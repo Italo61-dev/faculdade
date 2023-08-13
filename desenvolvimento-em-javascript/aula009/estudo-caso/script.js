73ce340438da4629135b6ea5120bc3c358aa52c1
Italo61-dev <italosousa42541@gmail.com>
2023-08-13 14:48:09 -0300

    Update script.js

--- desenvolvimento-em-javascript/aula009/estudo-caso/script.js
+++ desenvolvimento-em-javascript/aula009/estudo-caso/script.js
@@ -1,13 +1,19 @@
-var msgNome = document.get("#nomesobrenome");
- 
+var validacao = document.querySelector("#msgValidacao");
+var campoNome = document.querySelector("#inome");
+var campoEmail = document.querySelector("#iemail");
 
- 
 function validarNome(){
- 
-   if(msgNome.value.length == 0){
-      erro.textContent = "Nome é um campo Obrigatório";
-   }else{
-     erro.textContent = ""; 
-  }
- 
- };
+    if(campoNome.value.length == 0){
+        validacao.textContent = "Você não preencheu o campo nome";
+    }else{
+        validacao.textContent = " ";
+    }
+};
+
+function validarEmail(){
+    if(campoEmail.value.length == 0){
+        validacao.textContent = "Não esqueça de colocar seu email. Ele é muito importante."
+    }else{
+        validacao.textContent = " ";
+    }
+}
