fb02f6c4e13d01037e64c952f746fe29ec622d9f
Italo61-dev <italosousa42541@gmail.com>
2023-08-15 21:46:57 -0300

    Update ex001.js

--- exercicios/ex001.js
+++ exercicios/ex001.js
@@ -3,11 +3,22 @@
 
 // console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
 
-var n1 = 2
-var n2 = 5
+/////////////////////////////////////////
 
-soma = n1 + n2
-subtração = n1 - n2
-multiplicacao = n1 * n2
-divisão = n1 / n2
-console.log(soma, subtração, multiplicacao, divisão)
+// var n1 = 2
+// var n2 = 5
+
+// soma = n1 + n2
+// subtração = n1 - n2
+// multiplicacao = n1 * n2
+// divisão = n1 / n2
+// console.log(soma, subtração, multiplicacao, divisão)
+
+/////////////////////////////////////////
+var idade = 17
+
+if(idade >= 18){
+    console.log("Maior de 18")
+}else{
+    console.log("Menor de idade")
+}
