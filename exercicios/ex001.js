79599aec94106b11c054beb940e236c18f97ccc4
Italo61-dev <italosousa42541@gmail.com>
2023-08-15 22:17:56 -0300

    Update ex001.js

--- exercicios/ex001.js
+++ exercicios/ex001.js
@@ -1,9 +1,10 @@
+//Variáveis e tipos de dados: Escreva um programa que declare duas variáveis, “nome” e “idade”, e as imprima em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
 // const nome = "Italo"
 // const idade = 24
 
 // console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
 
-/////////////////////////////////////////
+//Operadores: Crie um programa que solicite ao usuário dois números e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
 
 // var n1 = 2
 // var n2 = 5
@@ -14,11 +15,54 @@
 // divisão = n1 / n2
 // console.log(soma, subtração, multiplicacao, divisão)
 
-/////////////////////////////////////////
-var idade = 17
+//Condições: Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade.
+// var idade = 17
 
-if(idade >= 18){
-    console.log("Maior de 18")
-}else{
-    console.log("Menor de idade")
-}
+// if(idade >= 18){
+//     console.log("Maior de 18")
+// }else{
+//     console.log("Menor de idade")
+// }
+
+//Loops: Crie um loop que imprima todos os números de 1 a 100.
+
+
+// for(i = 1 ; i <= 100; i++){
+//     console.log(i)
+// }
+
+//Funções: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+
+// function soma(num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(soma(8, 9))
+
+
+// Arrays: Escreva um programa que declare um array de suas frutas favoritas e imprima cada fruta na lista usando um loop.
+
+// const frutas = ["morango", "kiwi", "banana", "abacaxi"]
+
+// for(i = 0; i < frutas.length; i++){
+//     console.log(frutas[i])
+    
+// }
+
+//Objetos: Crie um objeto que represente um livro, com propriedades para título, autor e número de páginas. Imprima cada propriedade no console.
+
+livro = {
+    titulo: "A revolução",
+    autor: "George Orwell",
+    numeroDePaginas: 144
+};
+
+console.log(`título do livro é ${livro.titulo}, escrito pelo o autor ${livro.autor}, e o livro contêm ${livro.numeroDePaginas} páginas `)
+
+//Eventos DOM: Crie uma página HTML com um botão que, quando clicado, exiba um alerta que diga “Você clicou no botão!”.
+
+
+//Manipulação de Strings: Escreva uma função que receba uma string e retorne a mesma string, mas com a primeira letra de cada palavra em maiúsculas.
+
+
+//Promises e Fetch API: Use a Fetch API para fazer uma chamada para uma API pública (como a API do JSONPlaceholder) e exibir os resultados no console.
