9417f73159edf060400120dd7db90b8e6c45d9d3
Italo61-dev <italosousa42541@gmail.com>
2023-08-15 21:18:51 -0300

    Update ex001.js

--- exercicios/ex001.js
+++ exercicios/ex001.js
@@ -1,5 +1,13 @@
-const nome = "Italo"
-const idade = 24
+// const nome = "Italo"
+// const idade = 24
 
-console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
+// console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
 
+var n1 = 2
+var n2 = 5
+
+soma = n1 + n2
+subtração = n1 - n2
+multiplicacao = n1 * n2
+divisão = n1 / n2
+console.log(soma, subtração, multiplicacao, divisão)
